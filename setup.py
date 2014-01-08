a1266980493d416eadd5b790bb9c323dc8f009b4
Lucas <roesler.lucas@gmail.com>
2014-01-08 13:46:29 -0700

    Update contact info

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     name="imaper",
     version="1.0.1",
-    author="Dan Horrigan",
-    author_email="dan@dhorrigan.com",
-    url="https://bitbucket.org/dhrrgn/imaper",
+    author="Lucas Roesler",
+    author_email="lucas@jobdash.com",
+    url="https://github.com/jobdash/imaper",
     description="IMAP made easy.",
     long_description="See documentation at http://pythonhosted.org/imaper/",
     license='MIT',
